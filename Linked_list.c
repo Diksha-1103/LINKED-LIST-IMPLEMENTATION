7c980f15f45af2ee04e25e4c9f0711edd0649634
Diksha-1103 <dikshakumari1103@gmail.com>
2025-06-11 11:36:56 +0530

    Add files via upload

---
+++ Linked_list.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void deleteNode(struct Node** head, int key) {
+    struct Node* temp = *head;
+    struct Node* prev = NULL;
+
+    if (temp != NULL && temp->data == key) {
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL) return;
+
+    prev->next = temp->next;
+    free(temp);
+}
+
+void traverseList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtBeginning(&head, 10);
+    insertAtBeginning(&head, 4);
+    insertAtBeginning(&head, 17);
+    insertAtBeginning(&head, 23);
+    insertAtBeginning(&head, 1);
+    insertAtBeginning(&head, 35);
+    insertAtBeginning(&head, 14);
+
+
+    printf("Linked List after insertion: ");
+    traverseList(head);
+
+    
+    deleteNode(&head, 23);
+    printf("Linked List after deletion: ");
+    traverseList(head);
+
+    return 0;
+}
+
